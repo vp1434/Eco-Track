b86feeb1f5c660498c8edcf72e2b921aee7d8f88
Bikki <bikkikumarpandit2@gmail.com>
2026-02-17 00:55:23 +0530

    server.proxy remove

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
-  }
+  // server: {
+  //   port: 3000,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:5000',
+  //       changeOrigin: true
+  //     }
+  //   }
+  // }
 })
